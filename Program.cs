efcb90268c08ba121f7daca770181afd0d162b3c
Ram <ramprakash-s@outlook.com>
2021-06-28 15:33:32 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MagazineStore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
